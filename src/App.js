d99894dc77803a81f084febb902520e1a6505a88
JMazuryk <Julia.Mazuryk@gmail.com>
2022-08-06 15:08:42 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import MoviesPageMain from "./pages/MoviesPageMain";
 import MovieForm from "./components/movieForm/MovieForm";
 import Movies from "./components/movies/Movies";
-import Movie from "./components/movie/Movie";
 
 
 export default function App() {
